f4517b5a4d775b2c87e66203b9cba845f4b6e1ab
Abdul Jalil Miah <159704176+jalil035@users.noreply.github.com>
2024-10-16 04:38:06 +0600

    test 1

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,17 @@
 //navbear start
 let menuList = document.getElementById("menuList");
-        menuList.style.maxHeight = '0px';
+menuList.style.maxHeight = "0px";
 
-        function toggleMenu(){
-            if(menuList.style.maxHeight === '0px'){
-                menuList.style.maxHeight = '300px';
-            }else{
-                menuList.style.maxHeight = '0px';
-            }
-        }
+function toggleMenu() {
+  if (menuList.style.maxHeight === "0px") {
+    menuList.style.maxHeight = "300px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
 //navber end
 
-//Contact Us Form 
+//Contact Us Form
 const form = document.querySelector("form");
 let fullName = document.getElementById("name");
 let email = document.getElementById("email");
@@ -19,118 +19,111 @@ let phone = document.getElementById("phone");
 let subject = document.getElementById("subject");
 let message = document.getElementById("massage");
 
-function sendMail(){
+function sendMail() {
+  let messageBody = `Full Name: ${fullName.value}<br> EmailL ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
 
-    let messageBody = `Full Name: ${fullName.value}<br> EmailL ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "cc7.project2022@gmail.com",
-        Password : "7CEB9366732A96BBE6FAC52A90ACA01E2C57",
-        To : 'cc7.project2022@gmail.com',
-        From : "cc7.project2022@gmail.com",
-        Subject : subject.value,
-        Body : messageBody,
-    }).then(
-      message => {
-        if(message == "OK"){
-            Swal.fire({
-                title: "Success",
-                text: "Our Technical Team Contact You As Soon As Possible",
-                icon: "success"
-              });
-        }
-      }
-    );
-}
-
-function checkItem(){
-    let items = document.querySelectorAll(".item");
-
-    for(let item of items){
-        if(item.value == ""){
-            item.classList.add("error");
-            item.parentElement.classList.add("error");
-        }
-
-        item.addEventListener("keyup", () =>{
-            if(item.value != ""){
-                item.classList.remove("error");
-                item.parentElement.classList.remove("error");
-            }else{
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        })
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "cc7.project2022@gmail.com",
+    Password: "7CEB9366732A96BBE6FAC52A90ACA01E2C57",
+    To: "cc7.project2022@gmail.com",
+    From: "cc7.project2022@gmail.com",
+    Subject: subject.value,
+    Body: messageBody,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success",
+        text: "Our Technical Team Contact You As Soon As Possible",
+        icon: "success",
+      });
     }
+  });
 }
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
+function checkItem() {
+  let items = document.querySelectorAll(".item");
 
-    checkItem();
+  for (let item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
 
-    // sendMail();
-})
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkItem();
+});
 //Contact us From End
 
 //Accordion Start
 
-const accrodingBx = document.querySelectorAll(".accroding_bx")
+const accrodingBx = document.querySelectorAll(".accroding_bx");
 
 accrodingBx.forEach((item, index) => {
-    let titel = item.querySelector(".label");
-    let contents = item.querySelector(".content");
+  let titel = item.querySelector(".label");
+  let contents = item.querySelector(".content");
 
-    titel.addEventListener("click", () => {
-        item.classList.toggle('active');
+  titel.addEventListener("click", () => {
+    item.classList.toggle("active");
 
-        if(item.classList.contains("active")){
-            contents.style.height = `${contents.scrollHeight}px`
-            titel.style.background= "salmon"
-        }else{
-            contents.style.height = "0px"
-        }
-        removeOpen(index);
-        
-    })
-})
+    if (item.classList.contains("active")) {
+      contents.style.height = `${contents.scrollHeight}px`;
+      titel.style.background = "salmon";
+    } else {
+      contents.style.height = "0px";
+    }
+    removeOpen(index);
+  });
+});
 
-function removeOpen(index1){
-    accrodingBx.forEach((item2, index2) =>{
-        if(index1 != index2){
-            item2.classList.remove("active")
+function removeOpen(index1) {
+  accrodingBx.forEach((item2, index2) => {
+    if (index1 != index2) {
+      item2.classList.remove("active");
 
-        let des = item2.querySelector(".content")
-        des.style.height = "0px"
-        }
-    })
-};
+      let des = item2.querySelector(".content");
+      des.style.height = "0px";
+    }
+  });
+}
 //Accordion End
 
 //Image Filter Start
 let filterButton = document.querySelectorAll(".filter_button button");
-let filterableCard = document.querySelectorAll(".filter_card .gallery_card")
+let filterableCard = document.querySelectorAll(".filter_card .gallery_card");
 
-let filterCard = e => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active")
+let filterCard = (e) => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
 
-    filterableCard.forEach(card => {
-        card.classList.add("hide")
-        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
-            card.classList.remove("hide")
-        }
-    });
-    
+  filterableCard.forEach((card) => {
+    card.classList.add("hide");
+    if (
+      card.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
+      card.classList.remove("hide");
+    }
+  });
 };
 //Image Filter End
 
-
-
-
-
-filterButton.forEach(button => button.addEventListener("click", filterCard));
+//Counter Start
+filterButton.forEach((button) => button.addEventListener("click", filterCard));
 
 const counters = document.querySelectorAll(".counter");
 
@@ -147,3 +140,4 @@ counters.forEach((counter) => {
   };
   updateCounter();
 });
+//Counter End
